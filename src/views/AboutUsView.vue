61497131c557d3b92b549767d41a17d8cbdb1bd0
JordanAle <jsa242@gmail.com>
2024-05-22 18:13:24 -0400

    removed team section from about page

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -14,30 +14,6 @@ import Caption from '../components/Caption.vue'
       </div>
     </template>
   </Section>
-
-  <Section section_title="Meet The Team">
-    <template #section_content>
-      <AboutCard
-        img_uri="../assets/smiling_sloth.png"
-        title="Smiling Sloth The First"
-        text="May in Ayemenem is a hot, brooding month. The days are long and humid. The river shrinks and black crows gorge on bright mangoes in still, dustgreen trees. Red bananas ripen. Jackfruits burst. Dissolute bluebottles hum vacuously in the fruity air. Then they stun themselves against clear windowpanes and die, fatly baffled in the sun. The nights are clear, but suffused with sloth and sullen expectation. But by early June the southwest monsoon breaks and there are three months of wind and water with short spells of sharp, glittering sunshine that thrilled children snatch to play with. The countryside turns an immodest green. Boundaries blur as tapioca..."
-      >
-      </AboutCard>
-      <AboutCard
-        img_uri="../assets/smiling_sloth.png"
-        :left_text="true"
-        title="Smiling Sloth The Second"
-        text="May in Ayemenem is a hot, brooding month. The days are long and humid. The river shrinks and black crows gorge on bright mangoes in still, dustgreen trees. Red bananas ripen. Jackfruits burst. Dissolute bluebottles hum vacuously in the fruity air. Then they stun themselves against clear windowpanes and die, fatly baffled in the sun. The nights are clear, but suffused with sloth and sullen expectation. But by early June the southwest monsoon breaks and there are three months of wind and water with short spells of sharp, glittering sunshine that thrilled children snatch to play with. The countryside turns an immodest green. Boundaries blur as tapioca...May in Ayemenem is a hot, brooding month. The days are long and humid. The river shrinks and black crows gorge on bright mangoes in still, dustgreen trees. Red bananas ripen. Jackfruits burst."
-      >
-      </AboutCard>
-      <AboutCard
-        img_uri="../assets/smiling_sloth.png"
-        title="Smiling Sloth The Third"
-        text="May in Ayemenem is a hot, brooding month. The days are long and humid. The river shrinks and black crows gorge on bright mangoes in still, dustgreen trees. Red bananas ripen. Jackfruits burst. Dissolute bluebottles hum vacuously in the fruity air. Then they stun themselves against clear windowpanes and die, fatly baffled in the sun. The nights are clear, but suffused with sloth and sullen expectation. But by early June the southwest monsoon breaks and there are three months of wind and water with short spells of sharp, glittering sunshine that thrilled children snatch to play with. The countryside turns an immodest green. Boundaries blur as tapioca..."
-      >
-      </AboutCard>
-    </template>
-  </Section>
 </template>
 
 <style>
